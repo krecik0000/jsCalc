bd6536edfbe77a926dd317300a4de427140600f7
krecik0000 <modomoradzki@gmail.com>
2020-04-01 22:48:41 +0200

    th commit

--- calc.js
+++ calc.js
@@ -1,6 +1,3 @@
-window.onload = function() {
-    var num;
-    function insert(num){
-        document.forms.textview.value = document.forms.textview.value+num;
-    }    
-};
+//window.onload = function() {
+       
+//};
